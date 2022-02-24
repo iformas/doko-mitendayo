28166179a83ff2d2e32094939e5569d8b2925a96
iformas <iformas@hotmail.com>
2022-02-24 00:35:38 -0300

    .

--- app.py
+++ app.py
@@ -22,5 +22,6 @@ def create_app():
 
     return app
 
-    if __name__ == "__main__":
-        app.run(host='0.0.0.0', port=80, debug=True)
+if __name__ == '__main__':
+    app = create_app()
+    app.run(host='0.0.0.0', port=80, debug=True)    
